f24c39e18d37bd749218d3995fcb4d336fd19daa
Marcin Laube <laubemarcin2gmail.com>
2026-08-15 14:02:34 +0200

    .

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,42 +1,19 @@
-import java.io.;
-import java.util.;
+package core.basesyntax;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
 
 public class FileWork {
 
-    public static String[] readFromFile(String filename) {
-        List<String> result = new ArrayList<>();
+    public String[] readFromFile(String fileName) throws IOException {
+        String text = Files.readString(Path.of(fileName));
 
-        try (Scanner sc = new Scanner(new File(filename))) {
-
-            StringBuilder text = new StringBuilder();
-
-            while (sc.hasNextLine()) {
-                text.append(sc.nextLine()).append(" ");
-            }
-
-            String[] words = text.toString().split("[\\s\\p{Punct}]+");
-
-            for (String word : words) {
-                word = word.toLowerCase();
-
-                if (word.startsWith("w")) {
-                    result.add(word);
-                }
-            }
-
-        } catch (IOException e) {
-            return new String[0];
-        }
-
-        Collections.sort(result);
-
-        return result.toArray(new String[0]);
-    }
-
-    public static void main(String[] args) {
-
-        String[] result = readFromFile("liczby.txt");
-
-        System.out.println(Arrays.toString(result));
+        return Arrays.stream(text.split("[\\s\\p{Punct}]+"))
+                .map(String::toLowerCase)
+                .filter(word -> word.startsWith("w"))
+                .sorted()
+                .toArray(String[]::new);
     }
 }
