0222311f0a2ecf9ff0146a87771674d01533f73b
Marcin Laube <laubemarcin2gmail.com>
2026-08-19 10:48:12 +0200

    .

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,10 @@
 package core.basesyntax;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
         try {
