ef0bd0ee7a960e5ae6a1dcfa5686aeffe64f78d1
Marcin Laube <laubemarcin2gmail.com>
2026-08-19 10:27:40 +0200

    .

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,19 +1,8 @@
-package core.basesyntax;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Arrays;
-
-public class FileWork {
-
-    public String[] readFromFile(String fileName) throws IOException {
-        String text = Files.readString(Path.of(fileName));
-
-        return Arrays.stream(text.split("[\\s\\p{Punct}]+"))
-                .map(String::toLowerCase)
-                .filter(word -> !word.isEmpty() && word.startsWith("w"))
-                .sorted()
-                .toArray(String[]::new);
-    }
+public String[] readFromFile(String fileName) throws IOException {
+    String text = Files.readString(Path.of(fileName));
+    return Arrays.stream(text.split("[\\s\\p{Punct}]+"))
+            .map(String::toLowerCase)
+            .filter(word -> !word.isEmpty() && word.startsWith("w"))
+            .sorted()
+            .toArray(String[]::new);
 }
